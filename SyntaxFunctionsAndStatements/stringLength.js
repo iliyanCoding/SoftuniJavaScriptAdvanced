2777808e52585a38599d87842e0d60fe2781e998
iliyanCoding <iliyan.programirane@gmail.com>
2022-09-18 14:17:56 +0300

    Update stringLength.js

---
+++ SyntaxFunctionsAndStatements/stringLength.js
@@ -0,0 +1,6 @@
+function stringLength(firstString, secondString, thirdString){
+    let sumOfLength = firstString.length + secondString.length + thirdString.length;
+    let averageLength = Math.floor(sumOfLength / 3);
+    console.log(sumOfLength);
+    console.log(averageLength);
+}
