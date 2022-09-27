c3508b5136cb5df791e794934c45d26f7847f3ab
iliyanCoding <iliyan.programirane@gmail.com>
2022-09-27 19:44:12 +0300

    Update townPopulation.js

---
+++ ObjectsAndComposition/townPopulation.js
@@ -0,0 +1,14 @@
+function solve(townsArr){
+    let towns = {};
+    for(let townAsString of townsArr){
+        let [name, population] = townAsString.split(' <-> ');
+        population = Number(population);
+        
+        if(towns[name] != undefined) { population += towns[name]; }
+        towns[name] = population;
+    }
+
+    for (let town in towns){
+        console.log(`${town} : ${towns[town]} `)
+    }
+}
