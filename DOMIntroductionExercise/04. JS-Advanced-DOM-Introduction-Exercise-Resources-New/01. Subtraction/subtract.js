6aa9df0f274f886aecad9dbddcf048f1860d6ef0
iliyanCoding <iliyan.programirane@gmail.com>
2022-10-01 23:50:29 +0300

    Create subtract.js

---
+++ DOMIntroductionExercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
@@ -0,0 +1,8 @@
+function subtract() {
+    let num1 = document.getElementById('firstNumber').value;
+    let num2 = document.getElementById('secondNumber').value;
+
+    let result = Number(num1) - Number(num2);
+
+    document.getElementById('result').textContent = result;
+}
